5b29a156c0aa7f690eac3cc244af895d5867f8f7
Akash Singirikonda <akash@Akashs-MacBook-Pro.local>
2023-12-12 10:57:59 +0530

    added parser template

---
+++ src/scene/meshprimitive.h
@@ -0,0 +1,17 @@
+//#ifndef MESHPRIMITIVE_H
+//#define MESHPRIMITIVE_H
+
+//#include "shapeprimitive.h"
+
+//class MeshPrimitive : public ShapePrimitive
+//{
+//public:
+//    MeshPrimitive(glm::mat4 CTM, SceneMaterial material, std::string filepath);
+//    void updateParams(int param1, int param2);
+//private:
+//    void setVertexData();
+//    std::string m_filepath;
+//};
+
+//#endif // MESHPRIMITIVE_H
+
