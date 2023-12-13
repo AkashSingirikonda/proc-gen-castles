66cc8c4cd590d398935cff588f21141d642d86fa
Akash Singirikonda <akash@Akashs-MacBook-Pro.local>
2023-12-14 03:07:06 +0530

    Adding restarts to procedural generation, implemented parser to 3D shapes, added roof effects

--- src/procedural/proceduralcastle.cpp
+++ src/procedural/proceduralcastle.cpp
@@ -51,15 +51,22 @@ void ProceduralCastle::generateScene(Scene& scene)
     // Internal Walls
     tileStrings.push_back("WWWWWWWWW");
     // Single Sides, half walls
-    tileStrings.push_back("EWWEWWEWW");
     tileStrings.push_back("EEEWWWWWW");
-    tileStrings.push_back("WWEWWEWWE");
+    tileStrings.push_back("EWWEWWEWW");
     tileStrings.push_back("WWWWWWEEE");
+    tileStrings.push_back("WWEWWEWWE");
+
     // External Corners
-    tileStrings.push_back("EWWEWWEEE");
-    tileStrings.push_back("EEEEWWEWW");
     tileStrings.push_back("EEEWWEWWE");
+    tileStrings.push_back("EEEEWWEWW");
+
+    tileStrings.push_back("EWWEWWEEE");
     tileStrings.push_back("WWEWWEEEE");
+
+
+
+
+
 //    // Bridge Wall Connector:
 //    tileStrings.push_back("EWEWWWWWW");
 //    tileStrings.push_back("WWEWWWWWE");
@@ -101,8 +108,8 @@ void ProceduralCastle::generateScene(Scene& scene)
 
     // Spetting up grid for Wave Function Collapse
 
-    int width = 15;
-    int height = 15;
+    int width = 20;
+    int height = 20;
     bool finished_collapse;
     int num_restarts = 5;
     Grid wave = Grid(width, height, 0, options, tileMap);
@@ -208,25 +215,95 @@ void ProceduralCastle::generateScene(Scene& scene)
 
     // Adding in assets:
     SceneObject* wall = new SceneObject(PrimitiveType::WALL_HALF, TextureType::TEXTURE_STONE);
-    SceneObject* wallCorner = new SceneObject(PrimitiveType::WALL_CORNER_HALF_TOWER, TextureType::TEXTURE_STONE);
-    SceneObject* towerSquareRoof = new SceneObject(PrimitiveType::TOWER_SQUARE_BASE, TextureType::TEXTURE_STONE);
+    SceneObject* wallCorner = new SceneObject(PrimitiveType::WALL_CORNER_HALF, TextureType::TEXTURE_STONE);
+    SceneObject* towerSquareBase = new SceneObject(PrimitiveType::TOWER_SQUARE_BASE, TextureType::TEXTURE_STONE);
+    SceneObject* towerSquare = new SceneObject(PrimitiveType::TOWER_SQUARE, TextureType::TEXTURE_STONE);
+    SceneObject* towerSquareTopRoof = new SceneObject(PrimitiveType::TOWER_SQUARE_TOP_ROOF, TextureType::TEXTURE_ROOF);
+    // Mapping Sceneprimitives to their rotations and indices:
+
+    std::map<int, std::pair<SceneObject*, int>> to_rotate;
+
+    to_rotate[1].first = towerSquareBase;
+    to_rotate[1].second = 0;
+    for (int i = 0; i < 4; i++) {
+        to_rotate[i+2].first = wall;
+        to_rotate[i+2].second = i;
+        to_rotate[i+6].first = wallCorner;
+        to_rotate[i+6].second = i;
+    }
+
+    std::map<int, glm::vec3> offset_map;
+    offset_map[1] = glm::vec3{0, 0, 0};
+    offset_map[2] = glm::vec3{-1, 0, 1};
+    offset_map[3] = glm::vec3{-1, 0, 0};
+    offset_map[4] = glm::vec3{0, 0, 0};
+    offset_map[5] = glm::vec3{0, 0, 1};
+    offset_map[6] = glm::vec3{-1, 0, 1};
+    offset_map[7] = glm::vec3{-1, 0, 0};
+    offset_map[8] = glm::vec3{0, 0, 0};
+    offset_map[9] = glm::vec3{0, 0, 1};
+
+
 
     scene.root = root;
-
     // Creating an nxn graph:
-    int n = 5;
-    std::vector<SceneNode*> vals(n*n);
+    std::vector<SceneNode*> vals;
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            if (wave.m_grid[i][j]->m_collapsed) {
+                std::pair<SceneObject*, int> asset = to_rotate[wave.m_grid[i][j]->m_options[0].m_id];
+                if (wave.m_grid[i][j]->m_options[0].m_id != 0) {
+                    //SceneNode* to_insert3 = new SceneNode();
+//                    //std::cout << wave.m_grid[i][j]->m_options[0].m_id << std::endl;
+//                    to_insert3->objects.push_back(wallCorner);
+//                    to_insert3->transform = glm::scale(to_insert3->transform, glm::vec3(0.1, 0.1, 0.1));
+//                    to_insert3->transform = glm::translate(to_insert3->transform, glm::vec3(10*(i+1), 20, 10*(j-1)));
+//                    to_insert3->transform = glm::rotate(to_insert3->transform, glm::radians(-90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+//                    scene.root->children.push_back(to_insert3);
+//                    vals.push_back(to_insert3);
 
-    for (int i = 0; i < n*n; i++) {
-        vals[i] = new SceneNode();
-        //vals[i]->objects.push_back(cube);
-        //vals[i]->objects.push_back(plane);
-        vals[i]->objects.push_back(wall);
+                    SceneNode* to_insert2 = new SceneNode();
+                    //std::cout << wave.m_grid[i][j]->m_options[0].m_id << std::endl;
+                    to_insert2->objects.push_back(asset.first);
+                    //to_insert->transform = to_insert->transform * asset.second;
+                    to_insert2->transform = glm::scale(to_insert2->transform, glm::vec3(0.1, 0.1, 0.1));
+                    to_insert2->transform = glm::translate(to_insert2->transform,
+                                                           glm::vec3(10, 0, 10) * (glm::vec3(i, 0, j) + offset_map[wave.m_grid[i][j]->m_options[0].m_id]));
+                    to_insert2->transform = glm::rotate(to_insert2->transform, glm::radians(-90.0f * asset.second), glm::vec3(0.0f, 1.0f, 0.0f));
+                    scene.root->children.push_back(to_insert2);
+                    vals.push_back(to_insert2);
 
-        //vals[i]->objects.push_back(cone);
+                    if (wave.m_grid[i][j]->m_options[0].m_id == 1) {
+                        SceneNode* to_insert = new SceneNode();
+                        to_insert->objects.push_back(towerSquare);
+                        to_insert->transform = glm::scale(to_insert->transform, glm::vec3(0.1, 0.1, 0.1));
+                        to_insert->transform = glm::translate(to_insert->transform,
+                                                               glm::vec3(10, 10, 10) * (glm::vec3(i, 1, j) + offset_map[wave.m_grid[i][j]->m_options[0].m_id]));
+                        to_insert->transform = glm::rotate(to_insert->transform, glm::radians(-90.0f * asset.second), glm::vec3(0.0f, 1.0f, 0.0f));
+                        scene.root->children.push_back(to_insert);
+                        vals.push_back(to_insert);
 
-        vals[i]->transform = glm::translate(vals[i]->transform, glm::vec3(2 * (rand()/(float)RAND_MAX) * (i/n), 0, 2 * (rand()/(float)RAND_MAX) * (i % n)));
-        vals[i]->transform = glm::scale(vals[i]->transform, glm::vec3(0.1, 0.1, 0.1));
-        scene.root->children.push_back(vals[i]);
+                        SceneNode* to_insert3 = new SceneNode();
+                        //std::cout << wave.m_grid[i][j]->m_options[0].m_id << std::endl;
+                        to_insert3->objects.push_back(towerSquareTopRoof);
+                        //to_insert->transform = to_insert->transform * asset.second;
+                        to_insert3->transform = glm::scale(to_insert3->transform, glm::vec3(0.1, 0.1, 0.1));
+                        to_insert3->transform = glm::translate(to_insert3->transform,
+                                                              glm::vec3(10, 23, 10) * (glm::vec3(i, 1, j) + offset_map[wave.m_grid[i][j]->m_options[0].m_id]));
+                        to_insert3->transform = glm::rotate(to_insert3->transform, glm::radians(-90.0f * asset.second), glm::vec3(0.0f, 1.0f, 0.0f));
+                        scene.root->children.push_back(to_insert3);
+                        vals.push_back(to_insert3);
+                    }
+//                    SceneNode* to_insert = new SceneNode();
+//                    //std::cout << wave.m_grid[i][j]->m_options[0].m_id << std::endl;
+//                    to_insert->objects.push_back(towerSquareBase);
+//                    to_insert->transform = glm::scale(to_insert->transform, glm::vec3(0.1, 0.1, 0.1));
+//                    //to_insert->transform = glm::rotate(to_insert->transform, glm::radians(i * -90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+//                    to_insert->transform = glm::translate(to_insert->transform, glm::vec3(10*i, 10, 10*j));
+//                    scene.root->children.push_back(to_insert);
+//                    vals.push_back(to_insert);
+                }
+            }
+        }
     }
 }
